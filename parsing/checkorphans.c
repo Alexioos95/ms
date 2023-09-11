a64e91dc30b6b2ffc33c385b18a9e352f039c44b
Alexis PAYEN <apayen@made-f0Br6s6.clusters.42paris.fr>
2023-09-11 15:17:18 +0200

    Fixed all syntax errors.

--- parsing/checkorphans.c
+++ parsing/checkorphans.c
@@ -6,22 +6,33 @@
 /*   By: apayen <apayen@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/02 10:07:37 by apayen            #+#    #+#             */
-/*   Updated: 2023/09/11 11:03:39 by apayen           ###   ########.fr       */
+/*   Updated: 2023/09/11 15:15:50 by apayen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header.h"
 
+// Print l'erreur.
 int	printsyntaxerror(char *line, int i)
 {
 	if (line[i] == '\0')
 		printf("minishell: syntax error near unexpected token 'newline'\n");
 	else
-		printf("minishell: syntax error near unexpected token '%c'\n", \
-			line[i]);
+	{
+		if (i >= 1 && line[i - 1] == line[i])
+			printf("minishell: syntax error near unexpected token '%c%c'\n", \
+				line[i - 1], line[i]);
+		else if (i >= 1 && line[i] == line[i + 1])
+			printf("minishell: syntax error near unexpected token '%c%c'\n", \
+				line[i], line[i + 1]);
+		else
+			printf("minishell: syntax error near unexpected token '%c'\n", \
+				line[i]);
+	}
 	return (1);
 }
 
+// Cherche une redirection sans fichier.
 int	checkorphanredir(char *line)
 {
 	int		i;
@@ -64,8 +75,7 @@ int checkorphanpipe(char *line)
 				return (printsyntaxerror(line, i));
 			while (line[i] == ' ')
 				i++;
-			if (line[i] == '<' || line[i] == '>' || line[i] == '|' \
-				|| line[i] == '\0')
+			if (line[i] == '|' || line[i] == '\0')
 				return (printsyntaxerror(line, i));
 		}
 		i++;
